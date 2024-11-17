1038619d1a075504a52003b842b37697e38caa5a
naitea9527 <a22057916w@gmail.com>
2024-11-17 17:53:44 +0800

    Update iris.py

--- hw2/iris.py
+++ hw2/iris.py
@@ -6,32 +6,32 @@ from util import split_data, one_hot_encoding, plot_feature_scatter
 
 def load_iris_data(fp):
     data = np.genfromtxt(fp, delimiter=',', dtype=str)
-    feature_names = data[0, 1:5].tolist()
-    features = data[1:, 1:5].astype(float)   # convert the first 4 column to features
+    feature_names = data[0, 1:5].tolist()       # record the feature (column) names
+    features = data[1:, 1:5].astype(float)      # convert the first 4 column to features
     species, labels = np.unique(data[1:, 5], return_inverse=True)    # convert last column(species) to labels
-    return features, labels, dict(enumerate(species)), feature_names
+    return features, labels, len(species), dict(enumerate(species)), feature_names
 
 
 if __name__ == "__main__":
-    features, labels, species_to_label, feature_names = load_iris_data('Iris.csv')
+    features, labels, num_cls, species_to_label, feature_names = load_iris_data('data/Iris.csv')
 
-    plot_feature_scatter(features, labels, species_to_label, feature_names)
+    # plot feature-to-feature figures
+    plot_feature_scatter(features, labels, species_to_label, feature_names, save_dir="result/iris")
 
-    num_classes = len(species_to_label)
-    labels_one_hot = one_hot_encoding(labels, num_classes)
-    # train_features, train_labels, val_features, val_labels, test_features, test_labels = split_data(features, labels_one_hot)
+    # preprocess data
+    labels_one_hot = one_hot_encoding(labels, num_cls)
     X_train, X_val, X_test, y_train, y_val, y_test = split_data(features, labels_one_hot) 
 
 
     # Initialize and train the MLP network
     network = Network()
-    network.add_layer(Layer(input_size=4, output_size=5, activation=relu, activation_derivative=relu_derivative))
-    network.add_layer(Layer(input_size=5, output_size=5, activation=relu, activation_derivative=relu_derivative))
-    network.add_layer(Layer(input_size=5, output_size=3, activation=softmax))
+    network.add_layer(Layer(input_size=4, output_size=5, activation=relu))
+    network.add_layer(Layer(input_size=5, output_size=5, activation=relu))
+    network.add_layer(Layer(input_size=5, output_size=num_cls, activation=softmax))
 
 
     # network.train(train_features, train_labels, val_features, val_labels, learning_rate=0.01, epochs=1000)
-    network.train(X_train, y_train, X_val, y_val, learning_rate=0.01, epochs=1000, momentum=0.9)
+    network.train(X_train, y_train, X_val, y_val, learning_rate=0.01, epochs=1000, momentum=0.9, loss_type="categorical_cross_entropy")
 
 
     # Plotting accuracy vs epoch and loss vs epoch
